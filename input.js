d96bcb4b45096cae4113e4801b3afa34daba8b13
Buddie Chapman <buddiechapman4@gmail.com>
2020-08-21 19:16:33 -0500

    Can't make move when cursor is above or below the canvas

--- input.js
+++ input.js
@@ -1,20 +1,30 @@
 let selectedColumn = 0
+let mouseX = 0
+let mouseY = 100
 
 window.addEventListener("mousemove", function(evt){
 	if(gametray.getWinner() || gametray.isFull()) return
 	let columnWidth = TRAY_WIDTH / 7
 	mouseX = evt.clientX-canvas.getBoundingClientRect().x
+	mouseY = evt.clientY;
+	console.log(mouseY);
 	if(mouseX < TRAY_POS.x) mouseX = TRAY_POS.x
 	if(mouseX >= TRAY_POS.x + TRAY_WIDTH) mouseX = TRAY_POS.x + TRAY_WIDTH - 1
 	selectedColumn = Math.floor((mouseX-TRAY_POS.x) / columnWidth)
 	tokens[0].hold(selectedColumn)
 })
 
-window.addEventListener("mousedown", () => attemptPlay(selectedColumn))
+window.addEventListener("mousedown", () => {
+	let canvasRect = canvas.getBoundingClientRect();
+	if( contains({x:mouseX, y:mouseY}, canvasRect)){
+		attemptPlay(selectedColumn)
+	}
+})
 
 window.addEventListener("keydown", function(evt){
 	if(evt.key == 'u'){
 		undo()
+		undo();
 	}
 	if(evt.key == 'b'){
 		bot.easy(tokens[0].getColor())
@@ -23,3 +33,15 @@ window.addEventListener("keydown", function(evt){
 		resetGame()
 	}
 })
+
+function contains(point, rect){
+	if( point.x >= rect.x &&
+		point.x <= rect.width + rect.x &&
+		point.y >= rect.y &&
+		point.y <= rect.y + rect.height
+	){
+		return true
+	}else{
+		return false
+	}
+}
